d1b9863a16ea14a131f5fa67f9a94b7d96ffbd02
Francisco Guerrero <aguerrero@pivotal.io>
2020-06-08 04:55:00 -0700

    Properly configure the TaskExecutorPool

--- server/pxf-service/src/main/java/org/greenplum/pxf/service/PxfConfiguration.java
+++ server/pxf-service/src/main/java/org/greenplum/pxf/service/PxfConfiguration.java
@@ -1,13 +1,19 @@
 package org.greenplum.pxf.service;
 
 import org.greenplum.pxf.api.configuration.PxfServerProperties;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.task.TaskExecutorBuilder;
+import org.springframework.boot.task.TaskExecutorCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME;
 
 /**
  * Declares the registerSecurityServletFilter bean method to be processed by
@@ -15,44 +21,46 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 @EnableConfigurationProperties(PxfServerProperties.class)
-public class PxfConfiguration implements WebMvcConfigurer {
-
-    private final PxfServerProperties pxfServerProperties;
+public class PxfConfiguration {
 
     /**
-     * Constructs a PXF Configuration object with the provided PXF Server Properties
+     * Configures the TaskExecutorBuilder to be used for async requests (i.e. Bridge
+     * Read).
      *
-     * @param pxfServerProperties the server properties for PXF
+     * @return the {@link TaskExecutorBuilder} object
      */
-    public PxfConfiguration(PxfServerProperties pxfServerProperties) {
-        this.pxfServerProperties = pxfServerProperties;
+    @Bean
+    public TaskExecutorBuilder taskExecutorBuilder(PxfServerProperties pxfServerProperties,
+                                                   ObjectProvider<TaskExecutorCustomizer> taskExecutorCustomizers,
+                                                   ObjectProvider<TaskDecorator> taskDecorator) {
+        TaskExecutionProperties properties = pxfServerProperties.getTask();
+        TaskExecutionProperties.Pool pool = properties.getPool();
+        TaskExecutorBuilder builder = new TaskExecutorBuilder();
+        builder = builder.queueCapacity(pool.getQueueCapacity());
+        builder = builder.corePoolSize(pool.getCoreSize());
+        builder = builder.maxPoolSize(pool.getMaxSize());
+        builder = builder.allowCoreThreadTimeOut(pool.isAllowCoreThreadTimeout());
+        builder = builder.keepAlive(pool.getKeepAlive());
+        TaskExecutionProperties.Shutdown shutdown = properties.getShutdown();
+        builder = builder.awaitTermination(shutdown.isAwaitTermination());
+        builder = builder.awaitTerminationPeriod(shutdown.getAwaitTerminationPeriod());
+        builder = builder.threadNamePrefix(properties.getThreadNamePrefix());
+        builder = builder.customizers(taskExecutorCustomizers.orderedStream()::iterator);
+        builder = builder.taskDecorator(taskDecorator.getIfUnique());
+        return builder;
     }
 
-//    /**
-//     * Configures the TaskExecutor to be used for async requests (i.e. Bridge
-//     * Read).
-//     *
-//     * @return the {@link WebMvcConfigurer} object
-//     */
-//    @Override
-//    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-//        TaskExecutionProperties properties = pxfServerProperties.getTask();
-//        TaskExecutionProperties.Pool pool = properties.getPool();
-//        TaskExecutorBuilder builder = new TaskExecutorBuilder();
-//        builder = builder.queueCapacity(pool.getQueueCapacity());
-//        builder = builder.corePoolSize(pool.getCoreSize());
-//        builder = builder.maxPoolSize(pool.getMaxSize());
-//        builder = builder.allowCoreThreadTimeOut(pool.isAllowCoreThreadTimeout());
-//        builder = builder.keepAlive(pool.getKeepAlive());
-//        TaskExecutionProperties.Shutdown shutdown = properties.getShutdown();
-//        builder = builder.awaitTermination(shutdown.isAwaitTermination());
-//        builder = builder.awaitTerminationPeriod(shutdown.getAwaitTerminationPeriod());
-//        builder = builder.threadNamePrefix(properties.getThreadNamePrefix());
-//
-//        ThreadPoolTaskExecutor taskExecutor = builder.build(PxfThreadPoolTaskExecutor.class);
-//
-//        // TODO: make sure we hook to shutdown hook
-//        taskExecutor.initialize();
-//        configurer.setTaskExecutor(taskExecutor);
-//    }
+    /**
+     * Builds the {@link ThreadPoolTaskExecutor} that has previously been
+     * configured
+     *
+     * @param builder the {@link TaskExecutorBuilder} object
+     * @return the {@link ThreadPoolTaskExecutor} that has previously been configured
+     */
+    @Lazy
+    @Bean(name = {APPLICATION_TASK_EXECUTOR_BEAN_NAME,
+            AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME})
+    public ThreadPoolTaskExecutor applicationTaskExecutor(TaskExecutorBuilder builder) {
+        return builder.build(PxfThreadPoolTaskExecutor.class);
+    }
 }
